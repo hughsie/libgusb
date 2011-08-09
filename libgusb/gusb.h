2dac2e5ddc63e78d12bf43f412b6757487dc4c4b
Richard Hughes <richard@hughsie.com>
2011-08-09 16:49:34 +0200

    Require users specify G_USB_API_IS_SUBJECT_TO_CHANGE when using libusb

--- libgusb/gusb.h
+++ libgusb/gusb.h
@@ -22,6 +22,10 @@
 #ifndef __GUSB_H__
 #define __GUSB_H__
 
+#ifndef G_USB_API_IS_SUBJECT_TO_CHANGE
+#error You have to define G_USB_API_IS_SUBJECT_TO_CHANGE
+#endif
+
 #define __GUSB_INSIDE__
 
 #include <gusb/gusb-context.h>
