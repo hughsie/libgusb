0498825d25f112260e43bd519db995a2f7ed896e
Richard Hughes <richard@hughsie.com>
2011-08-11 11:28:19 +0200

    Add some self test code for GUsbDeviceList

--- gusb/gusb-self-test.c
+++ gusb/gusb-self-test.c
@@ -25,6 +25,8 @@
 
 #include "gusb-context.h"
 #include "gusb-source.h"
+#include "gusb-device.h"
+#include "gusb-device-list.h"
 
 static void
 gusb_context_func (void)
@@ -62,6 +64,63 @@ gusb_source_func (void)
 	g_object_unref (ctx);
 }
 
+static void
+gusb_device_list_func (void)
+{
+	GUsbContext *ctx;
+	GUsbDeviceList *list;
+	GError *error = NULL;
+	GPtrArray *array;
+	guint old_number_of_devices;
+	guint8 bus, address;
+	GUsbDevice *device;
+
+	ctx = g_usb_context_new (&error);
+	g_assert_no_error (error);
+	g_assert (ctx != NULL);
+
+	g_usb_context_set_debug (ctx, G_LOG_LEVEL_ERROR);
+
+	list = g_usb_device_list_new (ctx);
+	g_assert (list != NULL);
+
+	/* ensure we have an empty list */
+	array = g_usb_device_list_get_devices (list);
+	g_assert (array != NULL);
+	g_assert_cmpint (array->len, ==, 0);
+	g_ptr_array_unref (array);
+
+	/* coldplug, and ensure we got some devices */
+	g_usb_device_list_coldplug (list);
+	array = g_usb_device_list_get_devices (list);
+	g_assert (array != NULL);
+	g_assert_cmpint (array->len, >, 0);
+	old_number_of_devices = array->len;
+	g_ptr_array_unref (array);
+
+	/* coldplug again, and ensure we did not duplicate devices */
+	g_usb_device_list_coldplug (list);
+	array = g_usb_device_list_get_devices (list);
+	g_assert (array != NULL);
+	g_assert_cmpint (array->len, ==, old_number_of_devices);
+	device = G_USB_DEVICE (g_ptr_array_index (array, 0));
+	bus = g_usb_device_get_bus (device);
+	address = g_usb_device_get_address (device);
+	g_ptr_array_unref (array);
+
+	/* ensure we can search for the same device */
+	device = g_usb_device_list_get_dev_by_bus_n_address (list,
+							     bus,
+							     address);
+	g_assert (device != NULL);
+	g_assert_cmpint (bus, ==, g_usb_device_get_bus (device));
+	g_assert_cmpint (address, ==, g_usb_device_get_address (device));
+	g_object_unref (device);
+
+	g_object_unref (list);
+	g_object_unref (ctx);
+}
+
 int
 main (int argc, char **argv)
 {
@@ -75,6 +134,7 @@ main (int argc, char **argv)
 	/* tests go here */
 	g_test_add_func ("/gusb/context", gusb_context_func);
 	g_test_add_func ("/gusb/source", gusb_source_func);
+	g_test_add_func ("/gusb/device-list", gusb_device_list_func);
 
 	return g_test_run ();
 }
