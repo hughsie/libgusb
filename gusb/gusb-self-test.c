b89db74e549351d54f96680e0c2efefb5acf42b2
Ting-Wei Lan <lantw@src.gnome.org>
2017-12-19 13:52:13 +0800

    Root hubs on FreeBSD have vid and pid set to zero
    This doesn't really make the test work on FreeBSD. You still have to
    comment out g_object_unref calls or it will hang in g_usb_context_dispose.
    
    The first problem is that libusb_handle_events never returns. It polls
    on an internal pipe called 'ctrl_pipe' with default timeout -1. It can
    only be unblocked by opening or closing a device, or doing some data
    transfer works. Even if we replace libusb_handle_events with the new
    libusb_handle_events_timeout_completed function, it can still hang for
    several seconds, depending on the timeout we specify.
    
    The second problem is that libusb_exit can block for up to 4 seconds.
    When libusb_hotplug_register_callback is called, it starts a thread
    which sleep 4 seconds before doing any work. libusb_exit waits for the
    thread and there is no way to interrupt the sleep.

--- gusb/gusb-self-test.c
+++ gusb/gusb-self-test.c
@@ -32,8 +32,15 @@ gusb_device_func (void)
 	g_assert_cmpint (array->len, >, 0);
 	device = G_USB_DEVICE (g_ptr_array_index (array, 0));
 
-	g_assert_cmpint (g_usb_device_get_vid (device), >, 0x0000);
-	g_assert_cmpint (g_usb_device_get_pid (device), >, 0x0000);
+	/* Root hubs on FreeBSD have vid and pid set to zero */
+#ifdef __FreeBSD__
+	if (g_usb_device_get_parent (device) != NULL) {
+#endif
+		g_assert_cmpint (g_usb_device_get_vid (device), >, 0x0000);
+		g_assert_cmpint (g_usb_device_get_pid (device), >, 0x0000);
+#ifdef __FreeBSD__
+	}
+#endif
 
 	g_ptr_array_unref (array);
 }
@@ -88,8 +95,14 @@ gusb_context_func (void)
 	for (i = 0; i < array->len; i++) {
 		device = G_USB_DEVICE (g_ptr_array_index (array, i));
 
-		g_assert_cmpint (g_usb_device_get_vid (device), >, 0x0000);
-		g_assert_cmpint (g_usb_device_get_pid (device), >, 0x0000);
+#ifdef __FreeBSD__
+		if (g_usb_device_get_parent (device) != NULL) {
+#endif
+			g_assert_cmpint (g_usb_device_get_vid (device), >, 0x0000);
+			g_assert_cmpint (g_usb_device_get_pid (device), >, 0x0000);
+#ifdef __FreeBSD__
+		}
+#endif
 
 		/* Needed for g_usb_device_get_string_descriptor below,
 		   not error checked to allow running basic tests without
