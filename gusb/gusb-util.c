a281690f5b56faa29fe34fbce6f06813727ed503
Richard Hughes <richard@hughsie.com>
2020-01-21 10:19:00 +0000

    Wrap the upstream libusb_strerror()
    We depend on a libusb to always define this.

--- gusb/gusb-util.c
+++ gusb/gusb-util.c
@@ -11,45 +11,8 @@
 
 #include "gusb-util.h"
 
-/* libusb_strerror is not going upstream in the foreseeable future because of
-   i18n worries, provide our own implementation for now, later this can
-   hopefully became just a wrapper of the upstream version */
 const gchar *
 g_usb_strerror (gint error_code)
 {
-    enum libusb_error error = error_code;
-
-    switch (error) {
-    case LIBUSB_SUCCESS:
-        return "Success";
-    case LIBUSB_ERROR_IO:
-        return "Input/output error";
-    case LIBUSB_ERROR_INVALID_PARAM:
-        return "Invalid parameter";
-    case LIBUSB_ERROR_ACCESS:
-        return "Access denied (insufficient permissions)";
-    case LIBUSB_ERROR_NO_DEVICE:
-        return "No such device (it may have been disconnected)";
-    case LIBUSB_ERROR_NOT_FOUND:
-        return "Entity not found";
-    case LIBUSB_ERROR_BUSY:
-        return "Resource busy";
-    case LIBUSB_ERROR_TIMEOUT:
-        return "Operation timed out";
-    case LIBUSB_ERROR_OVERFLOW:
-        return "Overflow";
-    case LIBUSB_ERROR_PIPE:
-        return "Pipe error";
-    case LIBUSB_ERROR_INTERRUPTED:
-        return "System call interrupted (perhaps due to signal)";
-    case LIBUSB_ERROR_NO_MEM:
-        return "Insufficient memory";
-    case LIBUSB_ERROR_NOT_SUPPORTED:
-        return "Operation not supported or unimplemented on this platform";
-    case LIBUSB_ERROR_OTHER:
-        return "Other error";
-    default:
-        break;
-    }
-    return "Unknown error";
+	return libusb_strerror (error_code);
 }
